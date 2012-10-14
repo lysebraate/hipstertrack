c80e2ba3ca87dba4fee63f61c664f05d79fa760d
Tobias Torrissen <tobiast@gmail.com>
2012-10-14 22:14:48 +0200

    store to mongodb

--- server/App.rb
+++ server/App.rb
@@ -1,64 +1,45 @@
 require 'sinatra'  
 require 'active_support/core_ext'
+require 'mongo_mapper'
 
-require_relative 'user'
+require_relative 'User'
 
-get '/' do  
-	 'Hello, World!'  
-end  
+MongoMapper.connection = Mongo::Connection.new('localhost',27017, :pool_size => 5)
+MongoMapper.database = 'fastlege'
 
 
 # get a known user 
 get '/users/:id' do  
-	 puts("getting user")
-
-	 User.new("123", "Tobias", "Torrissen", "tobiast@gmail.com", "98257822").to_json
+	puts("getting user with id " + params[:id])
+	user = User.find_by_id(params[:id])
+	user.to_json
 end  
-
 
 # get all users
 get '/users' do  
-	 puts("getting users")
-	 User.new("123", "Tobias", "Torrissen", "tobiast@gmail.com", "98257822").to_json
+ 	puts("getting user")	
+	users = User.all
+	users.to_json
 end  
-
-
-# update a known user
-put '/users/:id' do 
-	 puts("Updating user")
-
-end
 
 # create a new user 
 post '/users' do 
 	puts("creating user")
-	
-	data = JSON.parse(request.body.read.to_s)  
-	
 
-	puts(data)
+	userdata = JSON.parse(request.body.read.to_s)  
+	mongouser = User.new(userdata) 
+  	mongouser.save
+
+	puts(userdata)
 end
-
-
-# get subscription for changes with doctor
-get '/subsriptions' do
-
-
-end
-
 
 # get all subscriptions for changes with doctor
 get '/subsriptions' do
-
-
+	puts("getting subscriptions")
 end
-
 
 # create subscription for changes with doctor
 post '/subsriptions' do
-
+	puts("creating subscriptions")
 
 end
-
-
-
--- server/user.rb
+++ server/user.rb
@@ -1,2 +1,10 @@
-class User < Struct.new(:id, :firstname, :lastname, :email, :phonenumber)
+class User
+  include MongoMapper::Document
+
+  key :firstname, String
+  key :lastname, String 
+  key :email, String 
+  key :phonenumber, String 
+
+ # many :subscriptions
 end
