24cdbf29daa2b7c71c15185c47726c7465aaae01
Tobias Torrissen <tobiast@gmail.com>
2012-10-15 18:22:07 +0200

    saving subscriptions

--- server/app.rb
+++ server/app.rb
@@ -3,13 +3,12 @@ require 'active_support/core_ext'
 require 'mongo_mapper'
 
 require_relative 'User'
+require_relative 'Subscription'
+
 
 MongoMapper.connection = Mongo::Connection.new('localhost',27017, :pool_size => 5)
 MongoMapper.database = 'fastlege'
 
-get "/" do
-  File.read(File.join('../public', 'index.html'))
-end
 
 # get a known user 
 get '/users/:id' do  
@@ -30,19 +29,30 @@ post '/users' do
 	puts("creating user")
 
 	userdata = JSON.parse(request.body.read.to_s)  
-	mongouser = User.new(userdata) 
-  	mongouser.save
+	newuser = User.new(userdata) 
+  	newuser.save
 
 	puts(userdata)
 end
 
 # get all subscriptions for changes with doctor
-get '/subsriptions' do
-	puts("getting subscriptions")
+get '/users/:id/subscriptions' do
+	puts("get subscriptions for user with id " + params[:id])
+
+	[{ :doctorid => '1234', :userid => '1234' },{ :doctorid => '12345', :userid => '12345' }].to_json
 end
 
 # create subscription for changes with doctor
-post '/subsriptions' do
-	puts("creating subscriptions")
+post '/users/:id/subscriptions' do
+	puts("creating subscriptions for user with id " + params[:id])
+	subscriptionData = JSON.parse(request.body.read.to_s)  
+	puts(subscriptionData)
+	newsubscription = Subscription.new(subscriptionData)
+    newsubscription.userid = params[:id]
+  	newsubscription.save
+ end
 
+# get doctors in Oslo 
+get '/doctors' do
+	[{ :doctorid => '1234', :name => 'Dr.Dyrego' },{ :doctorid => '12345', :name => 'Dr.Shivago' },{ :doctorid => '12345', :name => 'Dr.No' }].to_json
 end
--- server/subscription.rb
+++ server/subscription.rb
@@ -1,2 +1,5 @@
-class Subscription < Struct.new(:userid, :doctorid)
+class Subscription
+        include MongoMapper::Document
+        key :userid, String
+        key :doctorid, String
 end
