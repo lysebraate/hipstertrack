00ca7a97d4d073332b37c2d8441055f3326391f0
Jan Henrik Gundelsby <jhg@knowit.no>
2012-10-23 12:37:13 +0200

    Struktur på app.rb og config for at det skal fungere med vagrant. Kjør nå bare shotgun i diret

--- server/app.rb
+++ server/app.rb
@@ -15,56 +15,60 @@ require_relative 'fastlege_update_helper'
 MongoMapper.connection = Mongo::Connection.new('localhost',27017, :pool_size => 5)
 MongoMapper.database = 'fastlege'
 
-#static content
-get "/" do
-  File.read(File.join('public', 'index.html'))
-end
 
-# get a known user 
-get '/users/:id' do  
-	puts("getting user with id " + params[:id])
-	user = User.find_by_id(params[:id])
-	user.to_json
-end  
+class App < Sinatra::Base
 
-# get all users
-get '/users' do  
- 	puts("getting user")	
-	users = User.all
-	users.to_json
-end  
+	#static content
+	get "/" do
+	  File.read(File.join('public', 'index.html'))
+	end
 
-# create a new user 
-post '/users' do 
-	puts("creating user")
-	userdata = JSON.parse(request.body.read)  
-	newuser = User.new(userdata) 
-  	newuser.save
-end
+	# get a known user 
+	get '/users/:id' do  
+		puts("getting user with id " + params[:id])
+		user = User.find_by_id(params[:id])
+		user.to_json
+	end  
 
-# get all subscriptions for changes with doctor
-get '/users/:id/subscriptions' do
-	puts("get subscriptions for user with id " + params[:id])
-	user = User.find_by_id(params[:id])
-	user.subscriptions.to_json
-end
+	# get all users
+	get '/users' do  
+	 	puts("getting user")	
+		users = User.all
+		users.to_json
+	end  
 
-# create subscription for changes with doctor
-post '/users/:id/subscriptions' do
-	puts("creating subscriptions for user with id " + params[:id])
-	user = User.find_by_id(params[:id])
-  puts user
-	subscriptionData = JSON.parse(request.body.read)  
-  puts subscriptionData
-	user.subscriptions.build(subscriptionData)
-  	user.save
-end
+	# create a new user 
+	post '/users' do 
+		puts("creating user")
+		userdata = JSON.parse(request.body.read)  
+		newuser = User.new(userdata) 
+	  	newuser.save
+	end
 
-# get doctors in Oslo. Remote call to fastlegetjeneste by JHG
-get '/doctors' do
-  content_type :json
-  f = File.open("doctors.json")
-  f.gets
-	#$base_url = "lit-bayou-7664.herokuapp.com"
-	#Net::HTTP.get($base_url, $kvinnerioslo_url)
+	# get all subscriptions for changes with doctor
+	get '/users/:id/subscriptions' do
+		puts("get subscriptions for user with id " + params[:id])
+		user = User.find_by_id(params[:id])
+		user.subscriptions.to_json
+	end
+
+	# create subscription for changes with doctor
+	post '/users/:id/subscriptions' do
+		puts("creating subscriptions for user with id " + params[:id])
+		user = User.find_by_id(params[:id])
+	  puts user
+		subscriptionData = JSON.parse(request.body.read)  
+	  puts subscriptionData
+		user.subscriptions.build(subscriptionData)
+	  	user.save
+	end
+
+	# get doctors in Oslo. Remote call to fastlegetjeneste by JHG
+	get '/doctors' do
+	  content_type :json
+	  f = File.open("doctors.json")
+	  f.gets
+		#$base_url = "lit-bayou-7664.herokuapp.com"
+		#Net::HTTP.get($base_url, $kvinnerioslo_url)
+	end
 end
